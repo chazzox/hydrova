5db12677d28ba8905a8c35ad9f100cd91cb4f02b
charlie <42206116+chazzox@users.noreply.github.com>
2020-09-12 15:05:49 +0100

    debugging 404 on build site

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,3 @@
 module.exports = {
-	plugins: [
-		`gatsby-plugin-typescript`,
-		`gatsby-plugin-react-helmet`,
-		`gatsby-plugin-sass`,
-		{
-			resolve: `gatsby-plugin-manifest`,
-			options: {
-				name: 'Hydrova',
-				short_name: 'Hydrova | High performance reddit client',
-				start_url: '/',
-				display: 'standalone',
-				crossOrigin: `use-credentials`
-			}
-		}
-	]
+	plugins: [`gatsby-plugin-typescript`, `gatsby-plugin-react-helmet`, `gatsby-plugin-sass`]
 };
