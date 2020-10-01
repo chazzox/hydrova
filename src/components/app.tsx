e1fe984b95598998bf1312a320a2e67f916068ee
charlie <caylott@outlook.com>
2020-10-01 02:27:57 +0100

    added new routes

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -5,12 +5,16 @@ import Home from './home/home';
 import Subreddit from './subreddit/subreddit';
 import Post from './post/post';
 import Sidebar from './sidebar/sidebar';
+import Submit from './submit/submit';
+import Settings from './settings/settings';
 
 const App: React.FC = () => {
 	return (
 		<HashRouter>
 			<Sidebar />
 			<Switch>
+				<Route path="/submit" render={() => <Submit />} />
+				<Route path="/settings" render={() => <Settings />} />
 				<Route path="/r/:subreddit" render={props => <Subreddit {...props} />} />
 				<Route path="/post/:permalink" render={props => <Post {...props} />} />
 				<Route path="/" render={() => <Home />} />
---
+++ src/components/settings/settings.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Settings = () => {
+	return (
+		<div id="contentContainer">
+			<h1>settings page</h1>
+		</div>
+	);
+};
+
+export default Settings;
