aca8fc5575fb331ef20e9ab5b72fa9150652281e
PINPAL <josh.hayes614@gmail.com>
2020-10-16 14:23:40 +0100

    Fix comment styles

--- src/components/comment/comment.tsx
+++ src/components/comment/comment.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
 
+import './comment.scss';
+
 const Comment = ({ comment }: { comment: any }) => {
 	return (
 		<>
 			<div className="comment">
-				<div className="commentContent">
-					<div
-						className="commentBody"
-						dangerouslySetInnerHTML={{
-							__html:
-								new DOMParser().parseFromString(comment.body_html, 'text/html')
-									.documentElement.textContent || ''
-						}}
-					/>
-					<div className="commentButtonMenu">
-						<button>Upvote</button>
-						<button>Downvote</button>
-					</div>
-				</div>
+				<div
+					className="commentBody"
+					dangerouslySetInnerHTML={{
+						__html:
+							new DOMParser().parseFromString(comment.body_html, 'text/html').documentElement
+								.textContent || ''
+					}}
+				/>
+				<button>Upvote</button>
+				<button>Downvote</button>
 
 				{comment.replies
 					? comment.replies.data.children.map((childComments: any, index: any) => {
