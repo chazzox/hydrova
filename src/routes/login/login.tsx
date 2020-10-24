9929eff0ac9e93bb8a5257fd6a52dc9e01f51845
charlie <42206116+chazzox@users.noreply.github.com>
2020-10-24 18:33:58 +0100

    added basic hover effect to login page

--- src/routes/login/login.tsx
+++ src/routes/login/login.tsx
@@ -12,34 +12,47 @@ const Login = () => {
 	const dispatch: AppDispatch = useDispatch();
 
 	return (
-		<div id="loginBox">
-			<img src={hydrovaSVG} alt="logo" id="logo" />
-			<div id="loginInfo">
-				<h1>Hydrova</h1>
-				<h2>High Performance Reddit Client</h2>
-			</div>
-			<div
-				onClick={() => {
-					let loginModal = window.open(
-						generateAuthenticationURL(),
-						'_blank',
-						'resizable,scrollbars,status'
-					);
-					let checkLoginModalClosed = setInterval(() => {
-						if (loginModal?.closed) {
-							dispatch(refreshAccessToken({ refresh_token: Cookies.getJSON('refresh_token') }));
-							clearInterval(checkLoginModalClosed);
-						}
-					}, 50);
-				}}
-				id="signIn"
-			>
-				Sign In
-			</div>
+		<div
+			id="loginBoxContainer"
+			onMouseMove={event => {
+				const rotY = (-window.innerWidth / 2 + event.pageX) / 15;
+				const rotX = (-window.innerHeight / 2 + event.pageY) / 15;
+				event.currentTarget.style.transform = `rotateY(${rotY}deg) rotateX(${rotX}deg)`;
+			}}
+			onMouseEnter={event => {}}
+			onMouseLeave={event => {}}
+		>
+			<div id="loginBox">
+				<img src={hydrovaSVG} alt="logo" id="logo" />
+				<div id="loginInfo">
+					<h1>Hydrova</h1>
+					<h2>High Performance Reddit Client</h2>
+				</div>
+				<div
+					onClick={() => {
+						let loginModal = window.open(
+							generateAuthenticationURL(),
+							'_blank',
+							'resizable,scrollbars,status'
+						);
+						let checkLoginModalClosed = setInterval(() => {
+							if (loginModal?.closed) {
+								dispatch(
+									refreshAccessToken({ refresh_token: Cookies.getJSON('refresh_token') })
+								);
+								clearInterval(checkLoginModalClosed);
+							}
+						}, 50);
+					}}
+					id="signIn"
+				>
+					Sign In
+				</div>
 
-			<a href="https://www.reddit.com/register">
-				<div id="signUp">Sign Up</div>
-			</a>
+				<a href="https://www.reddit.com/register">
+					<div id="signUp">Sign Up</div>
+				</a>
+			</div>
 		</div>
 	);
 };
