bba09312204c9084a64aa1f3f22ceeff703c3079
charlie <42206116+chazzox@users.noreply.github.com>
2020-10-24 22:12:58 +0100

    fixed scopes, changes to 3d login effect

--- src/routes/login/login.tsx
+++ src/routes/login/login.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import Cookies from 'js-cookie';
 
 import { AppDispatch } from '../../redux/reduxWrapper';
 import { refreshAccessToken } from '../../redux/authReducer';
 import hydrovaSVG from '../../assets/logo.svg';
-import './login.scss';
 import generateAuthenticationURL from '../../utils/generateLogin';
-import Cookies from 'js-cookie';
+
+import './login.scss';
 
 const Login = () => {
 	const dispatch: AppDispatch = useDispatch();
-
+	const [isMouseOn, setMouseOn] = useState(false);
 	return (
 		<div
 			id="loginBoxContainer"
@@ -19,14 +20,26 @@ const Login = () => {
 				const rotX = (-window.innerHeight / 2 + event.pageY) / 15;
 				event.currentTarget.style.transform = `rotateY(${rotY}deg) rotateX(${rotX}deg)`;
 			}}
-			onMouseEnter={event => {}}
-			onMouseLeave={event => {}}
+			onMouseEnter={event => {
+				setMouseOn(true);
+			}}
+			onMouseLeave={event => {
+				setMouseOn(false);
+				event.currentTarget.style.transform = `rotateY(0}deg) rotateX(0deg)`;
+			}}
 		>
 			<div id="loginBox">
-				<img src={hydrovaSVG} alt="logo" id="logo" />
 				<div id="loginInfo">
-					<h1>Hydrova</h1>
-					<h2>High Performance Reddit Client</h2>
+					<img
+						src={hydrovaSVG}
+						alt="logo"
+						id="logo"
+						style={isMouseOn ? { transform: 'translate(-50%) translateZ(100px)' } : {}}
+					/>
+					<h1 style={isMouseOn ? { transform: 'translateZ(100px)' } : {}}>Hydrova</h1>
+					<h2 style={isMouseOn ? { transform: 'translateZ(100px)' } : {}}>
+						High Performance Reddit Client
+					</h2>
 				</div>
 				<div
 					onClick={() => {
@@ -45,12 +58,15 @@ const Login = () => {
 						}, 50);
 					}}
 					id="signIn"
+					style={isMouseOn ? { transform: 'translateZ(40px)' } : {}}
 				>
 					Sign In
 				</div>
 
 				<a href="https://www.reddit.com/register">
-					<div id="signUp">Sign Up</div>
+					<div style={isMouseOn ? { transform: 'translateZ(40px)' } : {}} id="signUp">
+						Sign Up
+					</div>
 				</a>
 			</div>
 		</div>
