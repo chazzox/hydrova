7a2b823f53808bd070287616ca25c6c17c4647e4
chazzox <42206116+chazzox@users.noreply.github.com>
2020-11-12 15:55:06 +0000

    added banner image

--- src/components/subredditInfoBar/subredditInfoBar.tsx
+++ src/components/subredditInfoBar/subredditInfoBar.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import getProfileURL from '../../utils/imgQuerySplit';
 
 import './subredditInfoBar.scss';
 
 const subredditInfoBar = ({ infoBar }: { infoBar: AboutApiResponse }) => {
 	return (
 		<div id="subredditInfoBar">
+			<img id="banner" style={{ height: '15px' }} src={getProfileURL(infoBar.data.banner_background_image)} />
 			<div
 				dangerouslySetInnerHTML={{
 					__html:
