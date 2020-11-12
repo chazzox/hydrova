b8eca8794099dc3268f93e8c91363333adf2e51e
PINPAL <18197217+PINPAL@users.noreply.github.com>
2020-11-12 09:51:59 +0000

    Remove Scroll Effect

--- src/components/listing/listing.tsx
+++ src/components/listing/listing.tsx
@@ -11,31 +11,11 @@ const Listing = ({ postData }: { postData: string[] }) => {
 	// redux
 	const dispatch: AppDispatch = useDispatch();
 	const posts = useSelector((state: ReduxStateType) => state.post.posts);
-	const isDynamic = useSelector((state: ReduxStateType) => state.style.dynamicExpand);
-
-	// component state
-	const [currentTop, setCurrentTop] = useState<HTMLElement>();
-	const postContainerRef = useRef<HTMLDivElement>(null);
-
-	const scrollEvent = () => {
-		setCurrentTop(
-			Array.from(postContainerRef.current?.children || []).find(
-				el => el.getBoundingClientRect().top > 0
-			) as HTMLElement
-		);
-		if (currentTop)
-			currentTop.style.minHeight = 100 + (500 - currentTop.getBoundingClientRect().top) + 'px';
-	};
 
 	return (
 		<>
-			{currentTop && posts[currentTop?.id || ''] ? (
-				<VoteControls postContent={posts[currentTop?.id].postContent} />
-			) : (
-				<div className="voteControls" />
-			)}
-			<div id="contentContainer" className="home" onScroll={scrollEvent}>
-				<span ref={postContainerRef}>
+			<div id="contentContainer" className="home">
+				<span>
 					{postData.map((postId, index) => {
 						const post = posts[postId].postContent;
 						return (
--- src/routes/home/home.tsx
+++ src/routes/home/home.tsx
@@ -5,8 +5,6 @@ import { AppDispatch, ReduxStateType } from '../../redux/reduxWrapper';
 import { GET_LISTING } from '../../redux/postStore/postThunks';
 import Listing from '../../components/listing/listing';
 
-import './home.scss';
-
 const Home = () => {
 	const dispatch: AppDispatch = useDispatch();
 	const timeline = useSelector((state: ReduxStateType) => state.post.subredditKeys['/']?.postKeys);
