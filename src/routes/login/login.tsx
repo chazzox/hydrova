d00e99461100afdace0939e9088d15fa6bfd0ca4
chazzox <42206116+chazzox@users.noreply.github.com>
2020-11-14 01:11:33 +0000

    removed 3d hover from login

--- src/routes/login/login.tsx
+++ src/routes/login/login.tsx
@@ -1,70 +1,41 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useDispatch } from 'react-redux';
-import Cookies from 'js-cookie';
 
 import { AppDispatch } from '../../redux/reduxWrapper';
 import { refreshAccessToken } from '../../redux/authReducer';
-import generateAuthenticationURL from '../../utils/generateLogin';
-
 import hydrovaSVG from '../../assets/logo.svg';
 import './login.scss';
+import generateAuthenticationURL from '../../utils/generateLogin';
+import Cookies from 'js-cookie';
 
 const Login = () => {
 	const dispatch: AppDispatch = useDispatch();
-	const [isMouseOn, setMouseOn] = useState(false);
-	return (
-		<div
-			id="loginBoxContainer"
-			onMouseMove={event => {
-				const rotY = (-window.innerWidth / 2 + event.pageX) / 15;
-				const rotX = (-window.innerHeight / 2 + event.pageY) / 15;
-				event.currentTarget.style.transform = `rotateY(${rotY}deg) rotateX(${rotX}deg)`;
-			}}
-			onMouseEnter={event => {
-				setMouseOn(true);
-			}}
-			onMouseLeave={event => {
-				setMouseOn(false);
-				event.currentTarget.style.transform = `rotateY(0}deg) rotateX(0deg)`;
-			}}
-		>
-			<div id="loginBox">
-				<div id="loginInfo">
-					<img
-						src={hydrovaSVG}
-						alt="logo"
-						id="logo"
-						style={isMouseOn ? { transform: 'translate(-50%) translateZ(100px)' } : {}}
-					/>
-					<h1 style={isMouseOn ? { transform: 'translateZ(100px)' } : {}}>Hydrova</h1>
-					<h2 style={isMouseOn ? { transform: 'translateZ(100px)' } : {}}>High Performance Reddit Client</h2>
-				</div>
-				<div
-					onClick={() => {
-						let loginModal = window.open(
-							generateAuthenticationURL(),
-							'_blank',
-							'resizable,scrollbars,status'
-						);
-						let checkLoginModalClosed = setInterval(() => {
-							if (loginModal?.closed) {
-								dispatch(refreshAccessToken({ refresh_token: Cookies.getJSON('refresh_token') }));
-								clearInterval(checkLoginModalClosed);
-							}
-						}, 50);
-					}}
-					id="signIn"
-					style={isMouseOn ? { transform: 'translateZ(40px)' } : {}}
-				>
-					Sign In
-				</div>
 
-				<a href="https://www.reddit.com/register">
-					<div style={isMouseOn ? { transform: 'translateZ(40px)' } : {}} id="signUp">
-						Sign Up
-					</div>
-				</a>
+	return (
+		<div id="loginBox">
+			<img src={hydrovaSVG} alt="logo" id="logo" />
+			<div id="loginInfo">
+				<h1>Hydrova</h1>
+				<h2>High Performance Reddit Client</h2>
 			</div>
+			<div
+				onClick={() => {
+					let loginModal = window.open(generateAuthenticationURL(), '_blank', 'resizable,scrollbars,status');
+					let checkLoginModalClosed = setInterval(() => {
+						if (loginModal?.closed) {
+							dispatch(refreshAccessToken({ refresh_token: Cookies.getJSON('refresh_token') }));
+							clearInterval(checkLoginModalClosed);
+						}
+					}, 50);
+				}}
+				id="signIn"
+			>
+				Sign In
+			</div>
+
+			<a href="https://www.reddit.com/register">
+				<div id="signUp">Sign Up</div>
+			</a>
 		</div>
 	);
 };
