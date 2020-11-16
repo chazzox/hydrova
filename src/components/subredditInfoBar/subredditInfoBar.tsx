52e8cfdbaf3751d87d4a1e4979c61e9f9ab198ab
chazzox <42206116+chazzox@users.noreply.github.com>
2020-11-16 16:56:45 +0000

    changed the icon path in sidebar

--- src/components/subredditInfoBar/subredditInfoBar.tsx
+++ src/components/subredditInfoBar/subredditInfoBar.tsx
@@ -9,7 +9,7 @@ const subredditInfoBar = ({ infoBar }: { infoBar: AboutApiResponse }) => {
 		<div id="subredditInfoBar">
 			<div id="subredditHeader">
 				<img id="subredditBanner" src={getProfileURL(infoBar.data.banner_background_image)} />
-				<img id="subredditIcon" src={getProfileURL(infoBar.data.icon_img)} />
+				<img id="subredditIcon" src={getProfileURL(infoBar.data.community_icon)} />
 				<h1>{infoBar.data.url}</h1>
 				<p>{infoBar.data.subscribers} Subscribers</p>
 			</div>
