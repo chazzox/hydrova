74f4a80b9d878d28b26ab845af03cf431b544679
chazzox <42206116+chazzox@users.noreply.github.com>
2020-11-19 10:54:31 +0000

    add support for links

--- src/utils/getPostValues.ts
+++ src/utils/getPostValues.ts
@@ -20,7 +20,8 @@ export default function getValues(data: ChildData): post {
 		is_gallery: data.is_gallery,
 		media_meta: data?.media_metadata,
 		gallery_data: data?.gallery_data,
-		thumbnail: data.thumbnail
+		thumbnail: data.thumbnail,
+		url_overridden_by_dest: data.url_overridden_by_dest
 		//oembed: data.media.oembed.html // FIXME:
 	};
 }
