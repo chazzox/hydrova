dc8c0ae0680a266de5c15bbd901243813777f35c
chazzox <42206116+chazzox@users.noreply.github.com>
2021-01-06 15:41:16 +0000

    remvoed user site to begin implementing it into dashboard route

--- src/routes/app.tsx
+++ src/routes/app.tsx
@@ -5,24 +5,23 @@ import Sidebar from 'components/sidebar';
 import Dashboard from './dashboard';
 
 // lazy loading other components
-const Settings = lazy(() => import(/* webpackPrefetch: true */ './settings'));
-const Submit = lazy(() => import(/* webpackPrefetch: true */ './submit'));
+const Settings = lazy(() => import('./settings'));
+const Submit = lazy(() => import('./submit'));
 const Mail = lazy(() => import('./mail'));
-const User = lazy(() => import('./user'));
 
 const App: React.FC = () => {
 	return (
 		<HashRouter>
 			<Sidebar />
-			<Suspense fallback={<div>Loading...</div>}>
+			<Suspense fallback={() => <></>}>
 				<Switch>
 					<Route path="/submit" exact render={() => <Submit />} />
 					<Route path="/mail" exact render={() => <Mail />} />
 					<Route path="/settings" exact render={() => <Settings />} />
-					<Route path="/user/:userId" exact render={() => <User />} />
+
 					<Route
 						exact
-						path={['/', '/r/:subName', '/r/:subName/:postId', '/:postId']}
+						path={['/', '/r/:subName', '/r/:subName/:postId', '/:postId', '/user/:userId']}
 						render={props => <Dashboard navProps={props} />}
 					/>
 				</Switch>
