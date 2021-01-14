ff200f9d8c95493c05b533597fab0cef1a121585
chazzox <42206116+chazzox@users.noreply.github.com>
2021-01-14 16:09:47 +0000

    added proper api comment loading, styles are cursed tho

--- src/components/postComments.tsx
+++ src/components/postComments.tsx
@@ -1,124 +1,133 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import AutoSizer from 'react-virtualized-auto-sizer';
-import { FixedSizeNodeData, FixedSizeNodePublicState, FixedSizeTree } from 'react-vtree';
+import { FixedSizeNodePublicState, FixedSizeTree } from 'react-vtree';
 import { NodeComponentProps, TreeWalkerValue } from 'react-vtree/dist/es/Tree';
+
 import GenericButton from './genericButton';
 
-type TreeNode = Readonly<{
-	children: TreeNode[];
-	id: number;
+interface TreeData {
+	id: string;
+	isOpenByDefault: boolean;
+	isLeaf: boolean;
 	name: string;
-}>;
+	nestingLevel: number;
+}
 
-type TreeData = FixedSizeNodeData &
-	Readonly<{
-		isLeaf: boolean;
-		name: string;
-		nestingLevel: number;
-	}>;
-
-let nodeId = 0;
-
-const createNode = (depth: number = 0): TreeNode => {
-	const node: TreeNode = {
-		children: [],
-		id: nodeId,
-		name: `test-${nodeId}`
-	};
-
-	nodeId += 1;
-
-	if (depth === 2) {
-		return node;
-	}
-
-	for (let i = 0; i < 10; i++) {
-		node.children.push(createNode(depth + 1));
-	}
-
-	return node;
-};
-
-const rootNode = Array.from({ length: 10 }, () => createNode());
 const defaultTextStyle = { marginLeft: 10 };
 
 type NodeMeta = Readonly<{
 	nestingLevel: number;
-	node: TreeNode;
+	node: CommentData;
 }>;
 
-const getNodeData = (node: TreeNode, nestingLevel: number): TreeWalkerValue<TreeData, NodeMeta> => ({
+const getNodeData = (node: CommentData, nestingLevel: number): TreeWalkerValue<TreeData, NodeMeta> => ({
 	data: {
 		id: node.id.toString(),
 		isLeaf: node.children.length === 0,
 		isOpenByDefault: true,
-		name: node.name,
+		name: node.body_html,
 		nestingLevel
 	},
 	nestingLevel,
 	node
 });
 
-function* treeWalker(): TreeWalker {
-	// Step [1]: Define the root node of our tree. There can be one or
-	// multiple nodes.
-	for (let i = 0; i < rootNode.length; i++) {
-		yield getNodeData(rootNode[i], 0);
-	}
-
-	while (true) {
-		// Step [2]: Get the parent component back. It will be the object
-		// the `getNodeData` function constructed, so you can read any data from it.
-		const parent = yield;
-
-		for (let i = 0; i < parent.node.children.length; i++) {
-			// Step [3]: Yielding all the children of the provided component. Then we
-			// will return for the step [2] with the first children.
-			yield getNodeData(parent.node.children[i], parent.nestingLevel + 1);
-		}
-	}
+interface CommentData {
+	id: string;
+	body_html: string;
+	children: CommentData[];
 }
+
+const Comments = () => {
+	const [treeData, setTreeData] = useState<CommentData[]>([{ id: '0', body_html: 'loading', children: [] }]);
+
+	useEffect(() => {
+		fetch('https://www.reddit.com/r/headphones/comments/kwkjbb/there_is_no_other_choice_for_good_sound/.json', {
+			method: 'GET'
+		})
+			.then((res) => res.json())
+			.then((json: GeneralPostResponse) => {
+				console.log('res', json);
+				setTreeData(getStuff(json[1].data.children));
+			})
+			.catch((err) => console.log(err));
+	}, []);
+
+	// extracting the values out of each comment that we need to render, only happens when we initially receive the data
+	const getStuff = (commentApiResponse: PurpleChild[]): CommentData[] =>
+		commentApiResponse.map<CommentData>(({ data }) => ({
+			id: data.id,
+			body_html: data.body_html,
+			children: typeof data.replies != 'object' ? [] : getStuff(data.replies.data.children),
+			author: data.author,
+			test: data.created_utc
+		}));
+
+	const treeWalkerCall = useCallback(
+		function* treeWalker(): any {
+			for (let i = 0; i < treeData.length; i++) {
+				yield getNodeData(treeData[i], 0);
+			}
+			while (true) {
+				const parent = yield;
+				for (let i = 0; i < parent.node.children.length; i++) {
+					yield getNodeData(parent.node.children[i], parent.nestingLevel + 1);
+				}
+			}
+		},
+		[treeData]
+	);
+
+	return (
+		<>
+			<AutoSizer>
+				{({ height, width }) => (
+					<FixedSizeTree
+						async={true}
+						treeWalker={treeWalkerCall}
+						itemSize={150}
+						height={height}
+						width={width}
+					>
+						{Node}
+					</FixedSizeTree>
+				)}
+			</AutoSizer>
+		</>
+	);
+};
 
 const Node: React.FC<NodeComponentProps<TreeData, FixedSizeNodePublicState<TreeData>>> = ({
 	data: { isLeaf, name, nestingLevel },
 	isOpen,
 	style,
 	setOpen
-}) => (
-	<div
-		style={{
-			...style,
-			alignItems: 'center',
-			display: 'flex',
-			marginLeft: nestingLevel * 30 + (isLeaf ? 48 : 0),
-			width: 'auto'
-		}}
-	>
-		{!isLeaf && (
-			<>
-				<GenericButton
-					text={isOpen ? '-' : '+'}
-					clickEvent={() => setOpen(!isOpen)}
-					additionalStyles={{ textAlign: 'center', margin: '0em', width: 'auto' }}
-				/>
-			</>
-		)}
-		<div style={defaultTextStyle}>{name}</div>
-	</div>
-);
-
-type TreePresenterProps = Readonly<{
-	itemSize: number;
-}>;
-const Comments = () => {
+}) => {
 	return (
-		<AutoSizer disableWidth>
-			{({ height, width }) => (
-				<FixedSizeTree treeWalker={treeWalker} itemSize={30} height={height} width={width}>
-					{Node}
-				</FixedSizeTree>
+		<div
+			style={{
+				...style,
+				alignItems: 'center',
+				display: 'flex',
+				marginLeft: nestingLevel * 30 + (isLeaf ? 48 : 0),
+				width: 'auto'
+			}}
+		>
+			{!isLeaf && (
+				<>
+					<GenericButton
+						text={isOpen ? '-' : '+'}
+						clickEvent={() => setOpen(!isOpen)}
+						additionalStyles={{ textAlign: 'center', margin: '0em', width: 'auto' }}
+					/>
+				</>
 			)}
-		</AutoSizer>
+			<span
+				dangerouslySetInnerHTML={{
+					__html: new DOMParser().parseFromString(name, 'text/html').documentElement.textContent || ''
+				}}
+			/>
+		</div>
 	);
 };
 
