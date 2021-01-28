ec2c1dc5340c0a04779c1ceff8776c3fc4c6eca9
chazzox <42206116+chazzox@users.noreply.github.com>
2021-01-28 09:21:38 +0000

    added options to submit side for possible use of selector pill component

--- src/routes/submit.tsx
+++ src/routes/submit.tsx
@@ -4,6 +4,11 @@ const Submit = () => {
 	const [newPostContent, setNewPostContent] = useState('');
 	return (
 		<div className="contentContainer">
+			<div>
+				<p>Post</p>
+				<p>Drafts</p>
+				<p></p>
+			</div>
 			<div className="submitInputContainer">
 				<input type="text" value={newPostContent} onChange={(event) => setNewPostContent(event.target.value)} />
 			</div>
