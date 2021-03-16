9c0f2fefaa0a2c1113e443809633d032fe4023e7
chazzox <42206116+chazzox@users.noreply.github.com>
2021-03-16 17:33:43 +0000

    built up new project structure

---
+++ src/pages/404.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+const Error404Page: React.FC = () => <>TET</>;
+
+export default Error404Page;
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const Index = () => {
+	return <>kekw</>;
+};
+
+export default Index;
