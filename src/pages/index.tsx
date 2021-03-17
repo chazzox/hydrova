9e71cc6a00ece28418e52125b3cb99122d57c56c
chazzox <42206116+chazzox@users.noreply.github.com>
2021-03-17 18:19:11 +0000

    updated prettierrc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,11 +13,6 @@ const Index = () => {
 	return (
 		// @todo: rework the themes into something more coherency and clean, maybe improve types/data structure
 		<ThemeProvider theme={{ colors: themes.defaultDark, base: baseTheme }}>
-			<link rel="preconnect" href="https://fonts.gstatic.com" />
-			<link
-				href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-				rel="stylesheet"
-			/>
 			<Global />
 			<WelcomeBox topText="Hydrova" bottomText="High Performance Reddit Client">
 				<p>test</p>
