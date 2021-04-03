14e73ae984d1015176ee04578581183e07cb547e
chazzox <42206116+chazzox@users.noreply.github.com>
2021-04-03 18:45:09 +0100

    improved post styles

--- src/components/Listing.tsx
+++ src/components/Listing.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { navigate } from 'gatsby-link';
 import { generatePath, Link, useParams, useRouteMatch } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { Virtuoso } from 'react-virtuoso';
@@ -30,6 +31,56 @@ const Post = styled.div`
 	overflow: hidden;
 	font-size: 11pt;
 	display: flex;
+	cursor: pointer;
+`;
+
+const Thumbnail = styled.img`
+	border-radius: ${(props) => props.theme.base.borderRadiusPrimary}px;
+	justify-self: right;
+	min-width: 80px;
+	min-height: 80px;
+	max-height: 80px;
+	max-width: 80px;
+	object-fit: cover;
+	visibility: visible;
+`;
+
+const PostInfo = styled.div`
+	padding-right: ${(props) => props.theme.base.paddingPrimary}px;
+	& > h1 {
+		overflow: hidden;
+		text-overflow: ellipsis;
+		line-height: 1.5em;
+		height: 3em;
+		font-size: 11pt;
+		font-weight: 400;
+	}
+	& * {
+		visibility: visible;
+		text-decoration: none;
+	}
+	& > p {
+		color: ${(props) => props.theme.colors.tertiaryText};
+		margin-top: 0;
+		margin-bottom: ${(props) => props.theme.base.paddingPrimary})px;
+		white-space: nowrap;
+	}
+	& > p * {
+		margin-left: ${(props) => props.theme.base.paddingSecondary}px;
+	}
+`;
+
+const RoundedLink = styled(Link)`
+	background-color: ${(props) => props.theme.colors.secondaryAccentBackground};
+	padding-right: ${(props) => props.theme.base.paddingPrimary}px;
+	padding-left: ${(props) => props.theme.base.paddingPrimary}px;
+	color: ${(props) => props.theme.colors.secondaryText};
+	border-radius: 100px;
+	font-weight: 400;
+	&:hover {
+		background-color: ${(props) => props.theme.colors.primaryAccentBackground};
+		color: ${(props) => props.theme.colors.buttonTextColor};
+	}
 `;
 
 const Listing: React.FC<ListingProps> = ({ idKeys, fetchMore }) => {
@@ -55,35 +106,35 @@ const Item: React.FC<{ content: Post }> = ({ content }) => {
 	const { path } = useRouteMatch();
 
 	return (
-		<Link
-			to={generatePath(path, {
-				listingType: listingType ?? 'r',
-				listingName: listingName ?? content.subreddit,
-				postId: content.id
-			})}
+		<Post
+			id={content.id}
+			onClick={() => {
+				navigate(
+					'#' +
+						generatePath(path, {
+							listingType: listingType ?? 'r',
+							listingName: listingName ?? content.subreddit,
+							postId: content.id
+						})
+				);
+			}}
 		>
-			<object>
-				<Post id={content.id}>
-					<div className="postInfo roundedLinks">
-						<p>
-							<Link to={'/u/' + content.author}>{content.author}</Link>
-							<span>{formatTimeSince(timeSinceCurrent(content.created_utc))}</span>
-							<Link to={'/' + content.subreddit_name_prefixed}>{content.subreddit_name_prefixed}</Link>
-						</p>
-						<h1 className="postTitle">
-							{new DOMParser().parseFromString(content.title, 'text/html').documentElement.textContent}
-						</h1>
-					</div>
-					<div className="data">
-						{content.thumbnail && content.thumbnail.match(/(default)|(self)|(unknown)/) === null ? (
-							<img src={content.thumbnail} alt={`thumbnail for ${content.id}`} />
-						) : (
-							<Markdown />
-						)}
-					</div>
-				</Post>
-			</object>
-		</Link>
+			<PostInfo>
+				<p>
+					<RoundedLink to={'/u/' + content.author}>{content.author}</RoundedLink>
+					<span>{formatTimeSince(timeSinceCurrent(content.created_utc))}</span>
+					<RoundedLink to={'/' + content.subreddit_name_prefixed}>{content.subreddit_name_prefixed}</RoundedLink>
+				</p>
+				<h1 className="postTitle">
+					{new DOMParser().parseFromString(content.title, 'text/html').documentElement.textContent}
+				</h1>
+			</PostInfo>
+			{content.thumbnail && content.thumbnail.match(/(default)|(self)|(unknown)/) === null ? (
+				<Thumbnail src={content.thumbnail} alt={`thumbnail for ${content.id}`} />
+			) : (
+				<Markdown />
+			)}
+		</Post>
 	);
 };
 
