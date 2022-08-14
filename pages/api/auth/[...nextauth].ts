6429aaa7bbd00fed7f3fed9d455bc80d42b29f2f
chazzox <caylott@outlook.com>
2022-08-14 13:12:55 +0100

    feat: uwu whats this

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,28 @@
+import NextAuth, { NextAuthOptions } from "next-auth"
+import RedditProvider from "next-auth/providers/reddit";
+ 
+export const authOptions: NextAuthOptions = { 
+  providers: [
+ 
+    RedditProvider({
+      clientId: process.env.REDDIT_CLIENT_ID,
+      clientSecret: process.env.REDDIT_CLIENT_SECRET,
+      authorization: {
+        params: {
+          duration: 'permanent',
+        },
+      },
+    })
+  ],
+  theme: {
+    colorScheme: "light",
+  },
+  callbacks: {
+    async jwt({ token }) {
+      token.userRole = "admin"
+      return token
+    },
+  },
+}
+
+export default NextAuth(authOptions)
