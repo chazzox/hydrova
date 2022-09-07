76ca21649b8311905f5b32707d200f85da077016
chazzox <caylott@outlook.com>
2022-09-07 04:35:18 +0100

    feat: added log out button

--- pages/settings/account.tsx
+++ pages/settings/account.tsx
@@ -1,8 +1,16 @@
 import SettingsLayout from '@components/layouts/settingsLayout';
+import { signOut } from 'next-auth/react';
 import { NextPageWithLayout } from 'pages/_app';
 
 const Account: NextPageWithLayout = () => {
-	return <>settings</>;
+	return (
+		<>
+			<h2 className="text-lg font-bold text-white/80">Account</h2>
+			<button className="btn my-3 px-8" onClick={() => signOut()}>
+				Logout
+			</button>
+		</>
+	);
 };
 
 Account.getLayout = (page) => <SettingsLayout>{page}</SettingsLayout>;
