7691ebaad889fb5cd6633357ff89a808057cb439
chazzox <caylott@outlook.com>
2022-09-18 14:30:23 +0100

    feat: ssr index site

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,30 @@
 import Layout from '@components/layouts';
 import { unstable_getServerSession } from 'next-auth';
 import type { GetServerSidePropsContext } from 'next/types';
-import { listing } from 'utils/reddit';
+import { getListing } from 'utils/reddit';
 import { authOptions } from './api/auth/[...nextauth]';
 import type { NextPageWithLayout } from './_app';
 
+import { dehydrate, QueryClient, useQuery } from '@tanstack/react-query';
+import Link from 'next/link';
 import { Listing } from '../typings/reddit.d';
 
-const Index: NextPageWithLayout<Listing['data']> = (props) => {
-	// react query or some shit
+const Index: NextPageWithLayout<{ initialData: Listing; token: string }> = ({ token }) => {
+	const { data } = useQuery(['listing'], () => getListing(token));
+
 	return (
 		<div className="drawer-content flex flex-row items-center justify-center gap-3 p-3">
 			<div className="h-full flex-1 overflow-y-auto rounded-xl bg-base-300 p-3 shadow-lg">
-				{props.children.map(({ data }) => (
-					<>
-						<div className="h-20 w-full" key={data.id}>
-							<h3 className="font-bold text-white/60">{data.title}</h3>
-						</div>
-						<div className="divider m-0"></div>
-					</>
+				{data?.data.children.map(({ data }) => (
+					<Link href={`https://reddit.com/${data.permalink}`}>
+						<a target="_blank">
+							<div className="h-32 w-full" key={data.id}>
+								<h3 className="font-bold text-white/60">{data.title}</h3>
+								<p className="link">{data.url}</p>
+							</div>
+							<div className="divider m-0"></div>
+						</a>
+					</Link>
 				))}
 			</div>
 			<div className="h-full flex-1 rounded-xl bg-base-300 shadow-lg"></div>
@@ -31,12 +37,15 @@ Index.getLayout = (page) => <Layout>{page}</Layout>;
 export async function getServerSideProps(context: GetServerSidePropsContext) {
 	// @ts-ignore
 	const { accessToken } = await unstable_getServerSession(context.req, context.res, authOptions);
+	const queryClient = new QueryClient();
 
 	// some fetch of basic listing
 	if (accessToken) {
-		const result = await listing(accessToken);
+		await queryClient.prefetchQuery(['listing'], () =>
+			getListing(accessToken, { limit: '10' })
+		);
 		return {
-			props: result.data
+			props: { dehydratedState: dehydrate(queryClient), token: accessToken }
 		};
 	}
 }
--- utils/reddit.ts
+++ utils/reddit.ts
@@ -5,15 +5,28 @@ enum METHODS {
 	POST = 'POST'
 }
 
-type listing_call_sig = (accessToken: string, listingParams?: string) => Promise<Listing>;
-export const listing: listing_call_sig = async (accessToken, listParams = '') => {
+type listing_call_sig = (
+	accessToken: string,
+	searchParams?: { [key: string]: string }
+) => Promise<Listing>;
+export const getListing: listing_call_sig = async (
+	accessToken,
+	searchParams = { raw_json: '1' }
+) => {
 	const req_url = new URL('https://oauth.reddit.com/');
+
+	// adding any parameters to request url
+	Object.entries(searchParams).forEach(([k, v]) => {
+		req_url.searchParams.append(k, v);
+	});
+
 	const req = await fetch(req_url, {
 		method: METHODS.GET,
-		headers: { Authorization: `Bearer ${accessToken}` }
+		headers: { Authorization: `Bearer ${accessToken}`, Accept: 'application/json' }
 	});
 
 	const res = (await req.json()) as Listing;
+
 	return res;
 };
 
