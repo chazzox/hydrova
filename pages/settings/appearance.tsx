a42dd1b82d3609d1f8274b4c89155c963d04398d
chazzox <caylott@outlook.com>
2022-09-20 18:38:11 +0100

    feat:  better appearance styles

--- pages/settings/appearance.tsx
+++ pages/settings/appearance.tsx
@@ -7,13 +7,14 @@ const Appearance: NextPageWithLayout = () => {
 	return (
 		<>
 			<h2 className="text-lg font-bold text-white/80">Colors</h2>
-			<div>
-				{SELECTED_THEMES.map((v) => (
+			<div className="my-3 flex gap-8">
+				{SELECTED_THEMES.map((v, i) => (
 					<button
+						key={i}
 						data-theme={v}
 						data-set-theme={v}
 						data-act-class="btn-active"
-						className="btn btn-primary m-2 capitalize"
+						className="btn btn-primary w-36 capitalize"
 					>
 						{v}
 					</button>
