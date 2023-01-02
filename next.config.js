78e733513bf75da16055220074a2563aad3b04cf
chazzox <caylott@outlook.com>
2023-01-02 18:47:30 +0000

    fix: nextjs is bad :)

--- next.config.js
+++ next.config.js
@@ -2,9 +2,7 @@ const runtimeCaching = require('next-pwa/cache');
 const withPWA = require('next-pwa')({
 	dest: 'public',
 	disable: process.env.NODE_ENV !== 'production',
-	ignoreBuildErrors: true,
-
 	runtimeCaching
 });
 
-module.exports = withPWA({ ignoreBuildErrors: true });
+module.exports = withPWA({ typescript: { ignoreBuildErrors: true } });
