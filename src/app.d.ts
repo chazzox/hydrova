d61487f1efcd9db1335fc9b9e4d21231ab29de04
chazzox <caylott@outlook.com>
2023-04-13 18:32:52 +0100

    typescript reset perhaps

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,3 @@
 // See https://kit.svelte.dev/docs/types#app
-// for information about these interfaces
-declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface Platform {}
-	}
-}
 
-export {};
+import '@total-typescript/ts-reset';
