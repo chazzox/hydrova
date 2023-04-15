320214c4b7d4e0a621020b49c758227883185dbd
chazzox <caylott@outlook.com>
2023-04-15 16:42:09 +0100

    yoyoyoyoyoyoyoyoyoyoyo

---
+++ src/lib/components/post-preview.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let post_id: undefined | string;
+</script>
+
+<div>
+	{#if post_id}
+		<p>post preview for {post_id}</p>
+	{:else}
+		<p>no post id</p>
+	{/if}
+</div>
--- src/routes/(sidebar)/(app)/+layout.svelte
+++ src/routes/(sidebar)/(app)/+layout.svelte
@@ -1,12 +1,24 @@
-<script>
+<script lang="ts">
 	import Timeline from '$lib/components/timeline.svelte';
+	import { page } from '$app/stores';
+	import PostPreview from '$lib/components/post-preview.svelte';
+
+	$: console.log($page.params);
+
+	let listing_id: string | undefined;
 </script>
 
 <div class="drawer-content flex flex-col gap-5 p-3 lg:flex-row">
 	<div class="rounded-box h-full w-full flex-1 overflow-y-auto bg-base-300 p-2">
-		<Timeline />
+		<Timeline
+			listing_id={$page.params?.subreddit
+				? `r/${$page.params?.subreddit}`
+				: $page.params.user_id
+				? `user/${$page.params.user_id}`
+				: undefined}
+		/>
 	</div>
 	<div class="rounded-box flex h-full w-full flex-1 items-center justify-center bg-base-300">
-		<p class="text-center text-3xl font-bold">This is where the post you click on is</p>
+		<PostPreview post_id={$page.params?.post_id} />
 	</div>
 </div>
