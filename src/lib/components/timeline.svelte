f90ff96bb2dd7dbe23e664acc911d65f63aedb5f
chazzox <caylott@outlook.com>
2023-05-03 04:26:41 +0100

    fixing sidebar links

--- src/lib/components/timeline.svelte
+++ src/lib/components/timeline.svelte
@@ -8,7 +8,6 @@
 	export let user_id: string;
 
 	$: listing_id = subreddit_id ? `r/${subreddit_id}` : user_id ? `user/${user_id}` : 'r/all';
-
 	$: listingQuery = createQuery({
 		queryKey: ['timeline', listing_id],
 		queryFn: () => getSubredditListing(listing_id)
@@ -21,30 +20,21 @@
 	<p>Error</p>
 {:else if $listingQuery.isSuccess}
 	{#each $listingQuery.data.data.children as postPreviewData}
-		<a
-			href={`/${listing_id}/${postPreviewData.data.id}`}
-			class="flex h-28 w-full flex-row items-center px-3"
-		>
-			<div class="flex flex-1 flex-col">
-				<!-- fucking horrible nested link hack 😍 -->
-				<!-- svelte-ignore a11y-missing-attribute -->
-				<object>
-					<p>
-						<a href={`/user/${postPreviewData.data.author}`} class="rounded-xl bg-white/40 px-2">
-							{postPreviewData.data.author}
-						</a>
-						<span>{getDateDiffString(postPreviewData.data.created * 1000)}</span>
-						<a
-							href={`/${postPreviewData.data.subreddit_name_prefixed}`}
-							class="rounded-xl bg-white/40 px-2"
-						>
-							{postPreviewData.data.subreddit_name_prefixed}
-						</a>
-					</p>
-				</object>
-				<p class="flex-1 text-ellipsis">{postPreviewData.data.title}</p>
-			</div>
+		<a href={`/${listing_id}/${postPreviewData.data.id}`} class="h-28 w-full px-3">
+			<p>
+				<a href={`/user/${postPreviewData.data.author}`} class="rounded-xl bg-white/40 px-2">
+					{postPreviewData.data.author}
+				</a>
+				<span>{getDateDiffString(postPreviewData.data.created * 1000)}</span>
+				<a
+					href={`/${postPreviewData.data.subreddit_name_prefixed}`}
+					class="rounded-xl bg-white/40 px-2"
+				>
+					{postPreviewData.data.subreddit_name_prefixed}
+				</a>
+			</p>
 
+			<p class="flex-1 text-ellipsis">{postPreviewData.data.title}</p>
 			<PostIcon class="h-20 w-20 rounded-lg bg-white/10 p-2" />
 		</a>
 		<div class="divider m-0" />
---
+++ src/routes/(sidebar)/mail/+page.svelte
@@ -0,0 +1,3 @@
+<div class="drawer-content flex flex-1 p-6">
+	<div class="rounded-box flex-1 bg-base-300" />
+</div>
